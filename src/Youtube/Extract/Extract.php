985accc0bc83024551f2b6bca9ccabc57155f5a5
mi6crazyheart <mi6crazyheart@yahoo.co.in>
2016-08-31 12:32:15 +0530

    update SRC folder content

---
+++ src/Youtube/Extract/Extract.php
@@ -0,0 +1,23 @@
+<?php namespace Youtube\Extract;
+
+class Extract {
+
+	public function validateUrl($url)
+	{
+		$urlData = parse_url($url);
+		if($urlData["host"] == 'www.youtube.com')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function greeting()
+	{
+		return "Good Morning";
+	}
+
+}
